626ea18f84a6c1130e2edfca8df94593087268fd
Mateo Lopez santibañez <mathewsls1002@gmail.com>
2025-04-15 21:55:02 -0500

    implement deploy

--- hotel_system/settings.py
+++ hotel_system/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%@++!pxe169=*0(7)&y%oy&874u=6v(ow)g*_1)%g$3vpwxn**
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["hoteleskevin.onrender.com"]
 
 
 # Application definition
